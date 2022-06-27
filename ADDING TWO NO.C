c917beb4c035b9c8f8227d761eb2bd1d6d40d281
MayankV127 <107644395+MayankV127@users.noreply.github.com>
2022-06-27 23:49:29 +0530

    Add files via upload

---
+++ ADDING TWO NO.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#inclue<conio.h>
+void main(){
+    int a,b,c;
+    clrscr();
+    printf("enter 2 number");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("addition is %d",c);
+    getch();
+}
